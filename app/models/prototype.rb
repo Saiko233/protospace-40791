9467537585531ba4ac64e89a325c89bf04421419
saiko233 <luffy123824778@gmail.com>
2024-05-15 13:36:49 +0900

    Imagick

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,6 +5,9 @@ class Prototype < ApplicationRecord
 
   validates :title, presence: true
   validates :catch_copy, presence: true
-  validates :concept, presence: true
+  validates :content, presence: true, unless: :was_attached?
 
+  def was_attached?
+    self.image.attached?
+  end
 end
